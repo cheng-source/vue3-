953110bd2bb0f9999fd48db84fcdffd495978f63
cjc <431909623@qq.com>
2022-12-31 21:18:40 +0800

    fix: 修复分支切换的bug

--- main.js
+++ main.js
@@ -1,36 +1,63 @@
 // 当读取对象属性时，把副作用函数放进桶里；当设置属性时，把副作用函数从桶里取出并执行
 let affectFunction = null;
-
+// 注册副作用函数的函数
 function effect(fn) {
-    affectFunction = fn;
-    fn();
+    function effectFn() {
+        clearFn(effectFn);
+        affectFunction = effectFn;
+        fn();
+    }
+    effectFn.relySet = [];
+    console.log(fn);
+    effectFn(fn);
 }
-const bucket = new WeakMap();
-const data = { text: 'hello world' };
+
+function clearFn(effectFn) {
+    for (let rely of effectFn.relySet) {
+        rely.delete(effectFn);
+    }
+    effectFn.relySet.length = 0;
+}
+
+const bucket = new WeakMap(); //当代理的对象不要时，该对象不会再使用，因此该及时回收对象，使用WeakMap不影响对象的垃圾回收
+
+// 追踪函数
+function track(target, key) {
+    if (!affectFunction) {
+        return target[key];
+    }
+    let keyMap = bucket.get(target);
+    if (!keyMap) {
+        bucket.set(target, (keyMap = new Map()));
+    }
+    let relySet = keyMap.get(key);
+    if (!relySet) {
+        keyMap.set(key, (relySet = new Set()));
+    }
+    relySet.add(affectFunction);
+    affectFunction.relySet.push(relySet);
+}
+
+// 触发函数
+function trigger(target, key) {
+    let keyMap = bucket.get(target);
+    if (!keyMap) {
+        return
+    }
+    let relySet = keyMap.get(key);
+    const effectToRun = new Set(relySet);
+    effectToRun.forEach(effectFn => effectFn());
+    // relySet && relySet.forEach((fn) => fn());
+}
+
+const data = { ok: true, text: 'hello world' };
 const obj = new Proxy(data, {
     get(target, key) {
-        if (!affectFunction) {
-            return target[key];
-        }
-        let keyMap = bucket.get(target);
-        if (!keyMap) {
-            bucket.set(target, (keyMap = new Map()));
-        }
-        let relySet = keyMap.get(key);
-        if (!relySet) {
-            keyMap.set(key, (relySet = new Set()));
-        }
-        relySet.add(affectFunction);
+        track(target, key);
         return target[key];
     },
     set(target, key, value) {
         target[key] = value;
-        let keyMap = bucket.get(target);
-        if (!keyMap) {
-            return
-        }
-        let relySet = keyMap.get(key);
-        relySet && relySet.forEach((fn) => fn());
-
+        trigger(target, key);
     }
 })
