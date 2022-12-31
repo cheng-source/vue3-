d5dcc5c9b11ff8922e7880f109165f440705de84
cjc <431909623@qq.com>
2022-12-31 16:48:53 +0800

    fix: 增加注册副作用函数的功能以及修改桶bucket结构

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 // 当读取对象属性时，把副作用函数放进桶里；当设置属性时，把副作用函数从桶里取出并执行
-const bucket = new Set();
+let affectFunction = null;
+
+function effect(fn) {
+    affectFunction = fn;
+    fn();
+}
+const bucket = new WeakMap();
 const data = { text: 'hello world' };
 const obj = new Proxy(data, {
     get(target, key) {
-        bucket.add(effect);
+        if (!affectFunction) {
+            return target[key];
+        }
+        let keyMap = bucket.get(target);
+        if (!keyMap) {
+            bucket.set(target, (keyMap = new Map()));
+        }
+        let relySet = keyMap.get(key);
+        if (!relySet) {
+            keyMap.set(key, (relySet = new Set()));
+        }
+        relySet.add(affectFunction);
         return target[key];
     },
     set(target, key, value) {
         target[key] = value;
-        bucket.forEach((fn) => fn());
-        return true;
+        let keyMap = bucket.get(target);
+        if (!keyMap) {
+            return
+        }
+        let relySet = keyMap.get(key);
+        relySet && relySet.forEach((fn) => fn());
+
     }
 })
